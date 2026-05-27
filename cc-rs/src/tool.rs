9bd149d97f3186e9f7fff906277ae583c74df896
aobaharuki2005 <tranbaohnth@outlook.com>
2026-05-28 04:30:32 +0700

    Patch: allow using default HashMap in cc-rs

--- cc-rs/src/tool.rs
+++ cc-rs/src/tool.rs
@@ -25,6 +25,7 @@ use crate::{
 /// compiler itself.
 #[derive(Clone, Debug)]
 #[allow(missing_docs)]
+#[allow(rustc::default_hash_types)]
 pub struct Tool {
     pub(crate) path: PathBuf,
     pub(crate) cc_wrapper_path: Option<PathBuf>,
