24c12e808bdd3303bfe7dda842e88ab6f7ba1f18
aobaharuki2005 <tranbaohnth@outlook.com>
2026-05-28 20:38:03 +0700

    Patches for 10.7 support (details see `patchset` folder)

---
+++ build.sh
@@ -0,0 +1,33 @@
+#!/bin/zsh
+
+### Setting up environment variables
+export SDKROOT=/Library/Developer/CommandLineTools/SDKs/MacOSX15.sdk
+export MACOSX_DEPLOYMENT_TARGET=10.7  # only needed if building targeted Rust toolchain
+export MACOSX_STD_DEPLOYMENT_TARGET=10.7  # only needed if building targeted Rust toolchain
+export LDFLAGS="-ld_classic"
+export VERSION=$(cat src/version)
+
+### Check if environment variables have been correctly set up
+echo "### Build environment variables: "
+printenv | grep -E "SDKROOT|MACOSX|LDFLAGS|VERSION"
+
+### Shallow synchronize submodules
+echo "====== STEP 1: Fetch submodules ====="
+echo "### Fetching ..."
+git submodule update --depth 1 --init --recursive
+
+### Build Rustc and cargo
+echo "====== STEP 2: Build Rustc and cargo ====="
+echo "### Symlink the configuration"
+ln -sf ./config-legacy.toml ./config.toml
+echo "### Here is the build configuration:"
+cat ./config.toml
+echo "### Starting to build ..."
+/usr/bin/python3 ./x.py build
+
+### Install and package
+echo "====== STEP 3: Install and package ====="
+/usr/bin/python3 ./x.py install
+cd build
+
+zip -qr $VERSION-custom-aarch64.zip ./$VERSION-custom-aarch64
