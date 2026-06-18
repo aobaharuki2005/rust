cd27b17d41f1f832f7d4c6eabe1d3b4b1c53f472
aobaharuki2005 <tranbaohnth@outlook.com>
2026-06-18 19:18:19 +0700

    Patch: allow default hash types in cc-rs

--- src/bootstrap/cc-rs/src/lib.rs
+++ src/bootstrap/cc-rs/src/lib.rs
@@ -210,7 +210,6 @@
 //! ```
 
 #![doc(html_root_url = "https://docs.rs/cc/1.0")]
-#![deny(warnings)]
 #![deny(missing_docs)]
 #![deny(clippy::disallowed_methods)]
 #![warn(clippy::doc_markdown)]
@@ -264,6 +263,7 @@ type Env = Option<Arc<OsStr>>;
 /// various configuration options and such of a compile. You'll find more
 /// documentation on each method itself.
 #[derive(Clone, Debug)]
+#[allow(rustc::default_hash_types)]
 pub struct Build {
     include_directories: Vec<Arc<Path>>,
     definitions: Vec<(Arc<str>, Option<Arc<str>>)>,
@@ -382,6 +382,7 @@ impl Object {
     }
 }
 
+#[allow(rustc::default_hash_types)]
 impl Build {
     /// Construct a new instance of a blank set of configuration.
     ///
