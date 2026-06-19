6e2a56172f856e2bbcb5238abb67c86121ff5623
aobaharuki2005 <tranbaohnth@outlook.com.vn>
2026-06-19 13:37:47 +0700

    Patch: fix custom build problem with cc-rs
    This patch is the combination of 2 patches:
    1. Patch: lower min deployment floor in cc-rs
    2. Patch: allow default hash types in cc-rs
    3. Fix: error: unreachable `pub` item

--- cc-rs/src/tool.rs
+++ cc-rs/src/tool.rs
@@ -1,3 +1,4 @@
+#![allow(rustc::default_hash_types)]
 use std::{
     borrow::Cow,
     collections::HashMap,
@@ -460,7 +461,7 @@ impl Tool {
 ///
 /// Detection of a family is done on best-effort basis and may not accurately reflect the tool.
 #[derive(Copy, Clone, Debug, PartialEq)]
-pub enum ToolFamily {
+pub(crate) enum ToolFamily {
     /// Tool is GNU Compiler Collection-like.
     Gnu,
     /// Tool is Clang-like. It differs from the GCC in a sense that it accepts superset of flags
